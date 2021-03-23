a0a3665825a510997d70af044468275fb08c5e50
Scott Kolodzieski <sckolo@mlc498.rmt.sas.com>
2021-03-23 18:04:44 -0400

    work on the h270n

---
+++ OC/h270/sub.sh
@@ -0,0 +1,3 @@
+sed      's/XXXXXXXXXXXXXXXXX/C02737701OPFF4KCB/g' <config-h270.plist >config.plist
+sed  -i  's/YYYYYYYYYYYY/C02VD2AJH7JY/g' config.plist
+#sed  -i  's/ZZZZZZZZ-ZZZZ-ZZZZ-ZZZZ-ZZZZZZZZZZZZ//g' config.plist 
